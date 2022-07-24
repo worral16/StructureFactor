207b3c06ccdd56c52d47706f96cf6719c718fd53
Worrad <worrada@udel.edu>
2022-07-24 11:55:42 -0400

    Comment added for clarity

--- fourierTransform.m
+++ fourierTransform.m
@@ -9,6 +9,7 @@ starter = 1;
 endval = 1;
 rdata = csgr0(:,1);
 grdata = csgr0(:,2);
+% Note: The rdata from the text file is normalized by the particle diameter 180 nm
 rdata = rdata(starter:end-endval);
 grdata = grdata(starter:end-endval);
 
@@ -24,6 +25,7 @@ sInc = sLow;
 sUpper = .02;
 scatteringAngleS = sLow:sInc:sUpper;
 structureFactor = zeros(1,length(scatteringAngleS));
+% convert rdata back to angstrom, user should modify this as needed
 rdata = rdata*1800;
 
 %% Numerical Fourier Transform Using Trapezoidal Integration
@@ -34,7 +36,7 @@ for s = sLow:sInc:sUpper
     for i = 1:length(grdata)
         y(i) = (grdata(i)-1) * sin(s*rdata(i))/(s*rdata(i))*rdata(i)^2;
     end
-    
+    % 10^-10 factor is for handling units in angstrom
     structureFactor(round(s*1/sLow)) = 1+2*pi*rho*trapz(rdata,y)*10^-10;
 end
 
